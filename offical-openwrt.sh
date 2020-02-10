acb72c5a258fb7421ac7c518dae23a83abb72bb7
SuLingGG <1215009714@qq.com>
2020-02-11 00:00:33 +0800

    Add x86_64 config & Test projet-openwrt offical openwrt

--- offical-openwrt.sh
+++ offical-openwrt.sh
@@ -8,12 +8,3 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# Change timezone
-cd ../..
-sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
-
-# Change default theme
-sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
-
-# Remove bootstrap theme
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
